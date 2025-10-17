44a8c02cfe6fe7d01b048710a0c545ac9dda2114
yahiasaad392 <20223161@fue.edu.eg>
2025-10-17 15:29:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,297 @@
 
          const quotes = [
-            {
-                quote: "“When you play the game of thrones, you win or you die.”",
-                author: "Cersei Lannister, Game of Thrones"
-            },
-            {
-                quote: "“Why so serious?”",
-                author: "The Joker, The Dark Knight"
-            },
-            {
-                quote: "“The greatest trick the Devil ever pulled was convincing the world he didn't exist.”",
-                author: "Roger 'Verbal' Kint, The Usual Suspects"
-            },
-            {
-                quote: "“Your mind is the scene of the crime.”",
-                author: "Cobb, Inception"
-            },
-            {
-                quote: "“The first rule of Fight Club is: you do not talk about Fight Club.”",
-                author: "Tyler Durden, Fight Club"
-            },
-            {
-                quote: "“What's in the box?”",
-                author: "Detective David Mills, Se7en"
-            },
-            {
-                quote: "“I'm the guy who does his job. You must be the other guy.”",
-                author: "Sgt. Dignam, The Departed"
-            },
-            {
-                quote: "“Sell me this pen.”",
-                author: "Jordan Belfort, The Wolf of Wall Street"
-            },
-            {
-                quote: "“A million dollars isn't cool. You know what's cool? A billion dollars.”",
-                author: "Sean Parker, The Social Network"
-            },
-            {
-                quote: "“I'm gonna make him an offer he can't refuse.”",
-                author: "Don Vito Corleone, The Godfather"
-            },
-            {
-                quote: "“Which would be worse – to live as a monster, or to die as a good man?”",
-                author: "Teddy Daniels, Shutter Island"
-            }
-        ];
+    // Original & Early Additions
+    {
+        quote: "“When you play the game of thrones, you win or you die.”",
+        author: "Cersei Lannister, Game of Thrones"
+    },
+    {
+        quote: "“Why so serious?”",
+        author: "The Joker, The Dark Knight"
+    },
+    {
+        quote: "“The greatest trick the Devil ever pulled was convincing the world he didn't exist.”",
+        author: "Roger 'Verbal' Kint, The Usual Suspects"
+    },
+    {
+        quote: "“Your mind is the scene of the crime.”",
+        author: "Cobb, Inception"
+    },
+    {
+        quote: "“The first rule of Fight Club is: you do not talk about Fight Club.”",
+        author: "Tyler Durden, Fight Club"
+    },
+    {
+        quote: "“What's in the box?”",
+        author: "Detective David Mills, Se7en"
+    },
+    {
+        quote: "“I'm the guy who does his job. You must be the other guy.”",
+        author: "Sgt. Dignam, The Departed"
+    },
+    {
+        quote: "“Sell me this pen.”",
+        author: "Jordan Belfort, The Wolf of Wall Street"
+    },
+    {
+        quote: "“A million dollars isn't cool. You know what's cool? A billion dollars.”",
+        author: "Sean Parker, The Social Network"
+    },
+    {
+        quote: "“I'm gonna make him an offer he can't refuse.”",
+        author: "Don Vito Corleone, The Godfather"
+    },
+    {
+        quote: "“Which would be worse – to live as a monster, or to die as a good man?”",
+        author: "Teddy Daniels, Shutter Island"
+    },
+    {
+        quote: "“Some men just want to watch the world burn.”",
+        author: "Alfred Pennyworth, The Dark Knight"
+    },
+    {
+        quote: "“Leave the gun. Take the cannoli.”",
+        author: "Peter Clemenza, The Godfather"
+    },
+    {
+        quote: "“It's only after we've lost everything that we're free to do anything.”",
+        author: "Tyler Durden, Fight Club"
+    },
+    {
+        quote: "“You talkin' to me?”",
+        author: "Travis Bickle, Taxi Driver"
+    },
+    {
+        quote: "“As far back as I can remember, I always wanted to be a gangster.”",
+        author: "Henry Hill, Goodfellas"
+    },
+    {
+        quote: "“People are having contests to see how much they can be like you.”",
+        author: "Gretchen, Gone Girl"
+    },
+    {
+        quote: "“You're not an asshole, Mark. You're just trying so hard to be.”",
+        author: "Erica Albright, The Social Network"
+    },
 
+    // Marvel & Spider-Man
+    {
+        quote: "“I am Iron Man.”",
+        author: "Tony Stark, Iron Man"
+    },
+    {
+        quote: "“That's my secret, Captain: I'm always angry.”",
+        author: "Bruce Banner, The Avengers"
+    },
+    {
+        quote: "“Dread it. Run from it. Destiny still arrives.”",
+        author: "Thanos, Avengers: Infinity War"
+    },
+    {
+        quote: "“I can do this all day.”",
+        author: "Steve Rogers, Captain America: The First Avenger"
+    },
+    {
+        quote: "“With great power comes great responsibility.”",
+        author: "Ben Parker, Spider-Man (2002)"
+    },
+    {
+        quote: "“We all have secrets: the ones we keep, and the ones that are kept from us.”",
+        author: "Peter Parker, The Amazing Spider-Man"
+    },
+    {
+        quote: "“You're not Superman, you know.”",
+        author: "Gwen Stacy, The Amazing Spider-Man 2"
+    },
+    {
+        quote: "“Sometimes you gotta run before you can walk.”",
+        author: "Tony Stark, Iron Man"
+    },
+    {
+        quote: "“The price of freedom is high... and it's a price I'm willing to pay.”",
+        author: "Steve Rogers, Captain America: The Winter Soldier"
+    },
+    {
+        quote: "“I'm with you 'til the end of the line.”",
+        author: "Steve Rogers, Captain America: Civil War"
+    },
+    {
+        quote: "“Avengers... Assemble.”",
+        author: "Steve Rogers, Avengers: Endgame"
+    },
+    {
+        quote: "“You know, I'm something of a scientist myself.”",
+        author: "Norman Osborn (Green Goblin), Spider-Man (2002)"
+    },
+    {
+        quote: "“Misery, misery, misery, that's what you've chosen.”",
+        author: "Norman Osborn (Green Goblin), Spider-Man (2002)"
+    },
+    {
+        quote: "“The power of the sun, in the palm of my hand.”",
+        author: "Dr. Otto Octavius, Spider-Man 2"
+    },
+
+    // Pitt & DiCaprio Movies
+    {
+        quote: "“You had my curiosity. But now you have my attention.”",
+        author: "Calvin Candie, Django Unchained"
+    },
+    {
+        quote: "“I'm not gonna die on this boat!”",
+        author: "Jordan Belfort, The Wolf of Wall Street"
+    },
+    {
+        quote: "“I have been a rich man and I have been a poor man. And I choose rich every f---ing time.”",
+        author: "Jordan Belfort, The Wolf of Wall Street"
+    },
+    {
+        quote: "“You're Rick f---ing Dalton. Don't you forget it.”",
+        author: "Rick Dalton, Once Upon a Time in Hollywood"
+    },
+    {
+        quote: "“I think this just might be my masterpiece.”",
+        author: "Lt. Aldo Raine, Inglourious Basterds"
+    },
+    {
+        quote: "“What's the first rule of Ocean's 11? Don't get caught.”",
+        author: "Danny Ocean, Ocean's Eleven"
+    },
+    {
+        quote: "“Immortality! Take it! It's yours!”",
+        author: "Achilles, Troy"
+    },
+
+    // More Iconic Movies & TV
+    {
+        quote: "“Say 'what' again. Say 'what' again, I dare you...”",
+        author: "Jules Winnfield, Pulp Fiction"
+    },
+    {
+        quote: "“I ate his liver with some fava beans and a nice Chianti.”",
+        author: "Dr. Hannibal Lecter, The Silence of the Lambs"
+    },
+    {
+        quote: "“There is no spoon.”",
+        author: "Spoon Boy, The Matrix"
+    },
+    {
+        quote: "“I have to return some videotapes.”",
+        author: "Patrick Bateman, American Psycho"
+    },
+    {
+        quote: "“I am not in danger, Skyler. I am the danger.”",
+        author: "Walter White, Breaking Bad"
+    },
+    {
+        quote: "“Say my name.”",
+        author: "Walter White, Breaking Bad"
+    },
+    {
+        quote: "“I am the one who knocks.”",
+        author: "Walter White, Breaking Bad"
+    },
+    {
+        quote: "“Yeah, science!”",
+        author: "Jesse Pinkman, Breaking Bad"
+    },
+    {
+        quote: "“Just because you shot Jesse James, don't make you Jesse James.”",
+        author: "Mike Ehrmantraut, Breaking Bad"
+    },
+    {
+        quote: "“Be a goldfish.”",
+        author: "Ted Lasso, Ted Lasso"
+    },
+    {
+        quote: "“Be curious, not judgmental.”",
+        author: "Ted Lasso, Ted Lasso"
+    },
+    {
+        quote: "“I believe in hope. I believe in Believe.”",
+        author: "Ted Lasso, Ted Lasso"
+    },
+    {
+        quote: "“Football is life!”",
+        author: "Dani Rojas, Ted Lasso"
+    },
+    {
+        quote: "“Smells like potential.”",
+        author: "Ted Lasso, Ted Lasso"
+    },
+
+    // Game of Thrones
+    {
+        quote: "“Winter is coming.”",
+        author: "Eddard Stark, Game of Thrones"
+    },
+    {
+        quote: "“I drink and I know things.”",
+        author: "Tyrion Lannister, Game of Thrones"
+    },
+    {
+        quote: "“Chaos isn't a pit. Chaos is a ladder.”",
+        author: "Petyr 'Littlefinger' Baelish, Game of Thrones"
+    },
+    {
+        quote: "“You know nothing, Jon Snow.”",
+        author: "Ygritte, Game of Thrones"
+    },
+    {
+        quote: "“What do we say to the God of Death? Not today.”",
+        author: "Syrio Forel, Game of Thrones"
+    },
+    {
+        quote: "“The man who passes the sentence should swing the sword.”",
+        author: "Eddard Stark, Game of Thrones"
+    },
+    {
+        quote: "“A Lannister always pays his debts.”",
+        author: "Tyrion Lannister, Game of Thrones"
+    },
+    {
+        quote: "“The night is dark and full of terrors.”",
+        author: "Melisandre, Game of Thrones"
+    },
+
+    // Final Big Addition
+    {
+        quote: "“Mankind was born on Earth. It was never meant to die here.”",
+        author: "Cooper, Interstellar"
+    },
+    {
+        quote: "“Do not go gentle into that good night; Rage, rage against the dying of the light.”",
+        author: "Professor Brand, Interstellar"
+    },
+    {
+        quote: "“Are you watching closely?”",
+        author: "Alfred Borden, The Prestige"
+    },
+    {
+        quote: "“Now I am become Death, the destroyer of worlds.”",
+        author: "J. Robert Oppenheimer, Oppenheimer"
+    },
+    {
+        quote: "“And like that... he's gone.”",
+        author: "Roger 'Verbal' Kint, The Usual Suspects"
+    },
+    {
+        quote: "“If you're a bird, I'm a bird.”",
+        author: "Noah, The Notebook"
+    },
+    {
+        quote: "“Okay? Okay.”",
+        author: "Hazel Grace & Augustus, The Fault in Our Stars"
+    },
+    {
+        quote: "“Thunder buddies for life!”",
+        author: "John & Ted, Ted"
+    },
+    {
+        quote: "“It's not a man-purse. It's called a satchel. Indiana Jones wears one.”",
+        author: "Alan, The Hangover"
+    }
+];
 
         const quoteBtn = document.getElementById('quoteBtn');
         const quoteOutput = document.getElementById('quoteOutput');
@@ -66,3 +312,4 @@
         }
 
         quoteBtn.addEventListener('click', generateQuote);
+
